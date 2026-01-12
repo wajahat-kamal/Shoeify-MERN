6b9cd8c916c2af4186f5fcf56f2935d02c44f184
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-01-12 14:48:56 +0500

    Update index.html to use DOCTYPE declaration and add favicon; refactor App component to include Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Home from "./page/Home";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <>
+      <Home />
+    </>
+  );
 }
 
-export default App
+export default App;
