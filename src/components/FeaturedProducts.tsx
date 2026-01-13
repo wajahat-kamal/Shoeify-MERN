22cdf395f29a304874cd9d65a123ea8691d5c1c0
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-01-13 18:59:33 +0500

    Add category filtering to FeaturedProducts component for enhanced product display

--- src/components/FeaturedProducts.tsx
+++ src/components/FeaturedProducts.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { ShoesData } from "../assets/ShoesData.js";
 import ProductCard from "./ProductCard.js";
 
 function FeaturedProducts() {
   const limitedShoes = ShoesData.slice(0, 8);
+
+  const [category, setCategory] = useState("All");
+  const categorys = ["All", "Men", "Women"];
+
+  const filterdShoes = ShoesData.filter(
+    (shoe) => category === "All" || shoe.category === category
+  );
+
   return (
     <section id="shopping" className="w-full py-24 px-6 md:px-20 bg-[#0B0F1A]">
       {/* Heading */}
@@ -17,9 +25,27 @@ function FeaturedProducts() {
         </p>
       </div>
 
+      <div className="flex flex-wrap items-center justify-center md:gap-6 gap-2 max-sm:gap-1.5 mb-6">
+        {categorys.map((category) => (
+          <button
+            key={category}
+            onClick={() => setCategory(category)}
+            className="relative
+                   px-4 py-1.5 md:px-4 md:py-1.5
+                   max-sm:px-2.5 max-sm:py-1      
+                   text-sm md:text-[15px] max-sm:text-xs  
+                   font-medium rounded-4xl overflow-hidden
+                   bg-gray-100 text-gray-600 shadow-sm
+                   transition-all duration-300"
+          >
+            {category}
+          </button>
+        ))}
+      </div>
+
       {/* Products Grid */}
       <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 px-6 md:px-0">
-        {limitedShoes.map((product) => (
+        {filterdShoes.map((product) => (
           <ProductCard product={product} />
         ))}
       </div>
