10e55dc984067f8597bea4a460e282d94645d54d
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-01-13 13:57:22 +0500

    Refactor Services component: replace hardcoded service cards with a dynamic array for improved maintainability and scalability.

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,35 +1,34 @@
 import { BadgeCheck, Headset, Truck, Wallet } from "lucide-react";
 
-function Services() {
+
+const servicesCards = [
+    {
+      heading: "Premium Quality",
+      para: "We provide only the best shoes for every lifestyle.",
+      Icon: BadgeCheck,
+    },
+    {
+      heading: "Free Delivery",
+      para: "Enjoy free shipping on all orders across the country.",
+      Icon: Truck,
+    },
+    {
+      heading: "ONLINE SUPPORT 24/7",
+      para: "24/7 friendly support for any questions or issues.",
+      Icon: Headset,
+    },
+    {
+      heading: "MONEY RETURN",
+      para: "Quick and easy returns with full money back.",
+      Icon: Wallet,
+    },
+  ];
+
+function Services() { 
+
   return (
     <div className="w-full p-6 md:px-20" id="services">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
-        {/* Card 1 */}
-        <div className="bg-[#1E2939] text-white p-6 rounded-2xl shadow-lg hover:scale-105 transition-transform duration-300">
-          <h3 className="text-xl font-semibold mb-2 flex flex-row gap-2">
-            <BadgeCheck /> Premium Quality
-          </h3>
-          <p>We provide only the best shoes for every lifestyle.</p>
-        </div>
-
-        {/* Card 2 */}
-        <div className="bg-[#1E2939] text-white p-6 rounded-2xl shadow-lg hover:scale-105 transition-transform duration-300">
-          <h3 className="text-xl font-semibold mb-2 flex flex-row gap-2">
-            <Truck />
-            Free Delivery
-          </h3>
-          <p>Enjoy free shipping on all orders across the country.</p>
-        </div>
-
-        {/* Card 3 */}
-        <div className="bg-[#1E2939] text-white p-6 rounded-2xl shadow-lg hover:scale-105 transition-transform duration-300">
-          <h3 className="text-xl font-semibold mb-2 flex flex-row gap-2">
-            <Headset />
-            ONLINE SUPPORT 24/7
-          </h3>
-          <p>24/7 friendly support for any questions or issues.</p>
-        </div>
-
         {/* Card 4 */}
         <div className="bg-[#1E2939] text-white p-6 rounded-2xl shadow-lg hover:scale-105 transition-transform duration-300">
           <h3 className="text-xl font-semibold mb-2 flex flex-row gap-2">
@@ -38,6 +37,9 @@ function Services() {
           </h3>
           <p>Quick and easy returns with full money back.</p>
         </div>
+
+          {servicesCards.map(())}
+        
       </div>
     </div>
   );
