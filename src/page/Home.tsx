2aed35ac8f4a5f547f25a2868be3729438015e43
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-01-13 13:51:03 +0500

    Update primary color in CSS and integrate Services component into Home page for improved layout and user engagement.

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import About from "../components/AboutUs";
+import Services from "../components/Services";
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <Navbar />
       <Hero />
       <About />
+      <Services/>
     </div>
   );
 }
