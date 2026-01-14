413eddce40715350e5c010268602bbec586400d0
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-01-14 14:46:27 +0500

    Add cart slice to Redux store with initial state and reducers for managing cart items

--- src/redux/cart/cartSlice.ts
+++ src/redux/cart/cartSlice.ts
@@ -1,2 +1,36 @@
 import { createSlice } from "@reduxjs/toolkit";
+import type { ShoeCard } from "../../assets/data/ShoesData";
 
+interface CartItem extends ShoeCard {
+  quantity: number;
+}
+
+interface CartState {
+  items: CartItem[];
+  isOpen: boolean;
+}
+
+const initialState: CartState = {
+  items: [],
+  isOpen: false,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart(state, action) {},
+    removeFromCart(state, action) {},
+
+    toggleCart(state) {
+      state.isOpen = !state.isOpen;
+    },
+    CloseCart(state) {
+      state.isOpen = false;
+    },
+  },
+});
+
+export const { addToCart, removeFromCart, toggleCart, CloseCart } =
+  cartSlice.actions;
+export default cartSlice.reducer;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-  },
+  reducer: {},
 });
 
 export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
