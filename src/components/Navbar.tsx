6bf881a646272348562877d4301d6220a23ba122
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-01-14 13:39:14 +0500

    Update Navbar component styles for improved visual consistency and hover effects

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -54,10 +54,10 @@ const Navbar: React.FC = () => {
               <a
                 key={name}
                 href={href}
-                className="relative font-mono text-md font-medium text-zinc-300 hover:text-white transition-colors group"
+                className="relative font-mono text-md font-medium text-zinc-200 hover:text-white transition-colors group"
               >
                 {name}
-                <span className="absolute bottom-0 left-0 h-0.5 w-0 text-white bg-white transition-all duration-300 group-hover:w-full" />
+                <span className="absolute bottom-0 left-0 h-0.5 w-0 bg-white transition-all duration-300 group-hover:w-full" />
               </a>
             ))}
           </div>
