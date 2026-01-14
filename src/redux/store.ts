c57cbe700f8fa96e29b238eecb4abde895c3db71
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-01-14 14:38:27 +0500

    Configure Redux store with cart reducer and define RootState and AppDispatch types for improved state management.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
-    reducer: {},
-})
+  reducer: {
+    cart: cartReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch
