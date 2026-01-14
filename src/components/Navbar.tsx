22b782a4ba18aff253f529d46d8d7bc14a6681f9
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-01-14 14:08:34 +0500

    Update package dependencies to include @reduxjs/toolkit, react-redux, and react-toastify for improved state management and notifications; adjust package-lock.json accordingly.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -60,7 +60,8 @@ const Navbar: React.FC = () => {
                 <span className="absolute bottom-0 left-0 h-0.5 w-0 bg-white transition-all duration-300 group-hover:w-full" />
               </a>
             ))}
-          </div>
+          </div
+          >
 
           {/* Mobile Toggle */}
           <button
