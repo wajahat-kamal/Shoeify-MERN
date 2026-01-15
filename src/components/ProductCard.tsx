4e9858b66288b276652f06e30f7ab4699cbd5283
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-01-15 07:38:38 +0500

    Refactor ProductCard component styles for improved responsiveness; adjust image height, text sizes, and button padding for better layout across devices.

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -22,7 +22,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ product, index }) => {
       className="group bg-(--secondary) border border-white/10 rounded-2xl relative overflow-hidden shadow-lg hover:shadow-xl transition-all duration-300"
     >
       {/* Image */}
-      <div className="relative w-full h-50 overflow-hidden rounded-t-2xl">
+      <div className="relative w-full h-30 md:h-50 overflow-hidden rounded-t-2xl">
         <img
           src={product.image}
           alt={product.name}
@@ -30,19 +30,19 @@ const ProductCard: React.FC<ProductCardProps> = ({ product, index }) => {
         />
 
         {/* Category Badge */}
-        <span className="absolute top-3 left-3 px-3 py-1 text-xs font-semibold rounded-full bg-(--primary) text-black">
+        <span className="absolute top-3 left-3 px-3 py-1 text-xs md:font-semibold rounded-full bg-(--primary) text-black">
           {product.category}
         </span>
       </div>
 
       {/* Product Info */}
-      <div className="p-3 flex flex-col gap-2">
+      <div className="p-1 md:p-3 flex flex-col gap-2">
         <div className="flex flex-row justify-between items-center ">
-          <h3 className="text-white font-semibold text-md line-clamp-1">
+          <h3 className="text-white font-semibold text-xs md:text-md line-clamp-1">
             {product.name}
           </h3>
 
-          <span className="text-(--primary) font-bold text-sm">
+          <span className="text-(--primary) md:font-bold text-xs">
             ${product.price}
           </span>
         </div>
@@ -51,8 +51,8 @@ const ProductCard: React.FC<ProductCardProps> = ({ product, index }) => {
         <button
           onClick={() => dispatch(addToCart(product))}
           className="
-    flex items-center justify-center gap-2 px-4 py-2
-    rounded-xl bg-(--primary)/80 text-white font-medium text-sm
+    flex items-center justify-center gap-2 px-2 md:px-4 py-1.5 md:py-2
+    rounded-xl bg-(--primary)/80 text-white font-medium text-xs md:text-sm
     w-full transition-all duration-300
     hover:bg-(--primary)/80 hover:scale-102
     active:scale-95
