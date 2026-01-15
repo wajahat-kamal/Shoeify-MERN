b762de7ac8764363cdb19540bc4b78d9cd945dab
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-01-15 15:11:09 +0500

    Refactor App component by removing unused imports for cleaner code and improved readability.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
 import Home from "./page/Home";
 import ProductsPage from "./page/ProductsPage";
 import Navbar from "./components/Navbar";
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Facebook, Twitter, Instagram } from "lucide-react";
 
 function Footer() {
